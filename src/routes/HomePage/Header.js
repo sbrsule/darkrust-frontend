e3dbc198297b85b4c0504db5169ab42e8bfb24a6
Sabr Suleiman <invictus@MacBook-Air.attlocal.net>
2022-04-13 17:36:01 -0400

    added logo to homepage

--- src/routes/HomePage/Header.js
+++ src/routes/HomePage/Header.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <div className="LandHeader">
+            <h2 className="Logo">DarkRust</h2>
+        </div>
+    )
+}
--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header.js"
 
 export default function HomePage() {
     return (
         <main>
-
+            <Header/>
         </main>
     )
 }
