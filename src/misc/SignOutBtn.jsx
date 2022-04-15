1f645d89fdf7bc6942f66475b2f262d6f0c0d2e6
Sol Invictus <invictus@Sabrs-Air.attlocal.net>
2022-04-14 20:17:31 -0400

    edited home buttons

---
+++ src/misc/SignOutBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import './SignOutBtn.css';
+
+export default function SignOutBtn() {
+    const navigate = useNavigate();
+
+    const logout = () => {
+        fetch("http://localhost:8080/logout", {
+            method: "POST",
+            credentials: "include",
+        })
+        .then((res) => {
+            if(res.status < 400) {
+                navigate("/");
+            }
+        })
+    }
+
+    return (
+        <button className="signout-btn" onClick={logout}>Sign Out</button>
+    )
+}
